7a9f03e6a710ae5b987337ee1a5b51469ad8f1c9
Areej <areejahmed.27502@gmail.com>
2024-08-03 18:37:35 +0300

    update

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
       // Creating a new button element for removing the task
       const removeButton = document.createElement('button');
       removeButton.textContent = "Remove";
-      removeButton.className = 'remove-btn';
+      removeButton.classList.add = 'remove-btn';
   
       // Connecting an onclick event to the remove button
       removeButton.onclick = function() {
